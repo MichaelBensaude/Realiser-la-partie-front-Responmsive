b8fde966e3c8edbe114cc2a5166477ffa999ef78
MichaelBensaude <m.bensaude@live.fr>
2025-06-06 20:14:13 +0200

    Implement routing and page structure for Quai Antique restaurant website

---
+++ Router/allRoutes.js
@@ -0,0 +1,11 @@
+import Route from './Route.js';
+// define all routes here
+export const allRoutes = [
+  new Route("/", "Accueil", "/pages/home.html"),
+];
+
+// le titre s affichera ici 
+
+export const websiteName = 'Quai Antique';
+
+
