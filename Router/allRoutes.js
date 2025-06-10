8e51ba5130910afcde1d33d90c544d70e2af0899
MichaelBensaude <77800651+MichaelBensaude@users.noreply.github.com>
2025-06-10 22:10:53 +0200

    Page de connexion

--- Router/allRoutes.js
+++ Router/allRoutes.js
@@ -2,7 +2,8 @@ import Route from './Route.js';
 // define all routes here
 export const allRoutes = [
   new Route("/", "Accueil", "/pages/home.html"),
-  new Route("/galerie", "Galerie", "/pages/galerie.html")
+  new Route("/galerie", "Galerie", "/pages/galerie.html"),
+  new Route("/signin", "Se Connecter", "/pages/signin.html"),
 ];
 
 // le titre s affichera ici 
