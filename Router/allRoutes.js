23de36d08cdb4e188c6530201f70536acc178dc1
MichaelBensaude <77800651+MichaelBensaude@users.noreply.github.com>
2025-06-15 21:23:00 +0200

    Gestion affichage des réservations

--- Router/allRoutes.js
+++ Router/allRoutes.js
@@ -6,7 +6,8 @@ export const allRoutes = [
   new Route("/signin", "Se Connecter", "/pages/auth/signin.html"),
   new Route("/signup", "S'inscrire", "/pages/auth/signup.html"),
   new Route("/account", "Mon Compte", "/pages/auth/account.html"),
-  new Route("/editPassword", "Modifier le mot de passe", "/pages/auth/editPassword.html")
+  new Route("/editPassword", "Modifier le mot de passe", "/pages/auth/editPassword.html"),
+  new Route("/reservations", "Mes Réservations", "/pages/reservations/allResa.html")
 ];
 
 // le titre s affichera ici 
