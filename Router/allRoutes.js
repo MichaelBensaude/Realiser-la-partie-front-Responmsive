d7dde0b474302ce22f6c0546dafe2d965e5b1860
MichaelBensaude <77800651+MichaelBensaude@users.noreply.github.com>
2025-06-16 09:43:46 +0200

    deplacement du fichier 404 et modification de allRoutes.js

--- Router/allRoutes.js
+++ Router/allRoutes.js
@@ -1,15 +1,17 @@
 import Route from './Route.js';
 // define all routes here
 
+const base = "/Realiser-la-partie-front-Responmsive";
+
 export const allRoutes = [
-  new Route("/Realiser-la-partie-front-Responmsive/", "Accueil", "pages/home.html"),
-  new Route("/Realiser-la-partie-front-Responmsive/galerie", "Galerie", "pages/galerie.html"),
-  new Route("/Realiser-la-partie-front-Responmsive/signin", "Se Connecter", "pages/auth/signin.html"),
-  new Route("/Realiser-la-partie-front-Responmsive/signup", "S'inscrire", "pages/auth/signup.html"),
-  new Route("/Realiser-la-partie-front-Responmsive/account", "Mon Compte", "pages/auth/account.html"),
-  new Route("/Realiser-la-partie-front-Responmsive/editPassword", "Modifier le mot de passe", "pages/auth/editPassword.html"),
-  new Route("/Realiser-la-partie-front-Responmsive/reservations", "Mes Réservations", "pages/reservations/allResa.html"),
-  new Route("/Realiser-la-partie-front-Responmsive/reservation", "Réserver une table", "pages/reservations/reserver.html")
+  new Route(`${base}/`, "Accueil", "pages/home.html"),
+  new Route(`${base}/galerie`, "Galerie", "pages/galerie.html"),
+  new Route(`${base}/signin`, "Se Connecter", "pages/auth/signin.html"),
+  new Route(`${base}/signup`, "S'inscrire", "pages/auth/signup.html"),
+  new Route(`${base}/account`, "Mon Compte", "pages/auth/account.html"),
+  new Route(`${base}/editPassword`, "Modifier le mot de passe", "pages/auth/editPassword.html"),
+  new Route(`${base}/reservations`, "Mes Réservations", "pages/reservations/allResa.html"),
+  new Route(`${base}/reservation`, "Réserver une table", "pages/reservations/reserver.html")
 ];
 
 // le titre s affichera ici 
