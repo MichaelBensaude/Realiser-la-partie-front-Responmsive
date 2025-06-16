afc11b8ac64567319391f10701db7546aecfad66
MichaelBensaude <77800651+MichaelBensaude@users.noreply.github.com>
2025-06-16 09:47:20 +0200

    downgrade des routes

--- Router/Route.js
+++ Router/Route.js
@@ -1,7 +1,8 @@
 export default class Route {
-  constructor(path, name, htmlFile) {
-    this.path = path;
-    this.name = name;
-    this.htmlFile = htmlFile;
-  }
+    constructor(url, title, pathHtml, pathJS = "") {
+      this.url = url;
+      this.title = title;
+      this.pathHtml = pathHtml;
+      this.pathJS = pathJS;
+    }
 }
--- Router/allRoutes.js
+++ Router/allRoutes.js
@@ -1,17 +1,14 @@
 import Route from './Route.js';
 // define all routes here
-
-const base = "/Realiser-la-partie-front-Responmsive";
-
 export const allRoutes = [
-  new Route(`${base}/`, "Accueil", "pages/home.html"),
-  new Route(`${base}/galerie`, "Galerie", "pages/galerie.html"),
-  new Route(`${base}/signin`, "Se Connecter", "pages/auth/signin.html"),
-  new Route(`${base}/signup`, "S'inscrire", "pages/auth/signup.html"),
-  new Route(`${base}/account`, "Mon Compte", "pages/auth/account.html"),
-  new Route(`${base}/editPassword`, "Modifier le mot de passe", "pages/auth/editPassword.html"),
-  new Route(`${base}/reservations`, "Mes Réservations", "pages/reservations/allResa.html"),
-  new Route(`${base}/reservation`, "Réserver une table", "pages/reservations/reserver.html")
+  new Route("/index.html", "Accueil", "/pages/home.html"),
+  new Route("/galerie", "Galerie", "/pages/galerie.html"),
+  new Route("/signin", "Se Connecter", "/pages/auth/signin.html"),
+  new Route("/signup", "S'inscrire", "/pages/auth/signup.html"),
+  new Route("/account", "Mon Compte", "/pages/auth/account.html"),
+  new Route("/editPassword", "Modifier le mot de passe", "/pages/auth/editPassword.html"),
+  new Route("/reservations", "Mes Réservations", "/pages/reservations/allResa.html"),
+  new Route("/reservation", "Réserver une table", "/pages/reservations/reserver.html")
 ];
 
 // le titre s affichera ici 
